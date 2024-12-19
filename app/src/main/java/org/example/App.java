edcb81abbe340fe4d1915f08861fd6af7b66b64b
Adom-code <jyaopoku@st.ug.edu.gh>
2024-12-19 17:25:05 +0300

    Updated app

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,7 +4,11 @@
 package org.example;
 
 public class App {
+
+    public static void main(String[] args){
+
     Book b001 = new Book("desert rivers","Ama Wilson",2023);
     System.out.println(b001.getTitle());
+    
     }
 }
